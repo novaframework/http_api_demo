18cc2a796b33122c681fb3487fb57c243941ac0f
Daniel Widgren <daniel.widgren@gmail.com>
2021-03-04 07:49:30 +0100

    updated to latest version of Nova and change routing to be explicit

--- priv/http_api_demo.routes.erl
+++ priv/http_api_demo.routes.erl
@@ -1,8 +1,11 @@
 #{prefix => "",
   security => false,
   routes => [
-            {"/pet", { http_api_demo_pet_controller, manage_pet}, #{methods => [options, get, post]}},
-            {"/pet/:petid", { http_api_demo_pet_controller, manage_pet}, #{methods => [options, get, put]}}
+            {"/pet", { http_api_demo_pet_controller, get_pets}, #{methods => [options, get]}},
+            {"/pet", { http_api_demo_pet_controller, create_pet}, #{methods => [options, post]}},
+            {"/pet/:petid", { http_api_demo_pet_controller, get_pet}, #{methods => [options, get]}},
+            {"/pet/:petid", { http_api_demo_pet_controller, update_pet}, #{methods => [options, put]}},
+            {"/pet/:petid", { http_api_demo_pet_controller, remove_pet}, #{methods => [options, delete]}}
            ],
  statics => [
              {"/assets/[...]", "assets"}
--- src/controllers/http_api_demo_pet_controller.erl
+++ src/controllers/http_api_demo_pet_controller.erl
@@ -1,33 +1,37 @@
 -module(http_api_demo_pet_controller).
--export([manage_pet/1]).
+-export([get_pet/1,
+         update_pet/1,
+         remove_pet/1,
+         get_pets/1,
+         create_pet/1]).
 
-manage_pet(#{req := #{method := <<"GET">>,
-                      bindings := #{petid := PetId}}}) ->
+get_pet(#{bindings := #{petid := PetId}}) ->
     case ets:lookup(pets, PetId) of
         [] ->
             {status, 404};
         [{PetId, Name}] ->
-            {status, 200, #{}, json:encode(#{<<"id">> => PetId,
-                                             <<"name">> => Name}, [maps, binary])}
-    end;
-manage_pet(#{req := #{method := <<"PUT">>,
-                      bindings := #{petid := PetId}},
+            {json, 200, #{}, #{<<"id">> => PetId,
+                               <<"name">> => Name}}
+    end.
+
+update_pet(#{bindings := #{petid := PetId},
              json := #{<<"name">> := Name}}) ->
     true = ets:delete(pets, PetId),
     true = ets:insert(pets, {PetId, Name}),
-    {status, 200, #{}, json:encode(#{<<"id">> => PetId,
-                                     <<"name">> => Name}, [maps, binary])};
-manage_pet(#{req := #{method := <<"DELETE">>,
-                      bindings := #{petid := PetId}}}) ->
+    {json, 200, #{}, #{<<"id">> => PetId,
+                       <<"name">> => Name}}.
+
+remove_pet(#{bindings := #{petid := PetId}}) ->
     true = ets:delete(pets, PetId),
-    {status, 200}; 
-manage_pet(#{req := #{method := <<"GET">>}}) ->
+    {status, 200}.
+
+get_pets(_) ->
     List = ets:tab2list(pets),
     Body = [#{<<"id">> => Id,
               <<"name">> => Name} ||{Id, Name} <- List],
-    {status, 200, #{}, json:encode(Body, [maps, binary])};
-manage_pet(#{req := #{method := <<"POST">>},
-             json := #{<<"name">> := Name}}) ->
+    {json, 200, #{}, Body}.
+
+create_pet(#{json := #{<<"name">> := Name}}) ->
     Id = list_to_binary(uuid:uuid_to_string(uuid:get_v4())),  
     true = ets:insert(pets, {Id, Name}),
-    {status, 201, #{}, json:encode(#{<<"id">> => Id, <<"name">> => Name}, [maps, binary])}.
+    {json, 201, #{}, #{<<"id">> => Id, <<"name">> => Name}}.
